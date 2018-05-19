9dca502f9369832e6bae2ec25f27ee9a5297bbfe
dongchaozhu <279949406@qq.com>
2018-05-19 17:59:24 +0800

    22

--- src/main/java/com/wsloan/redpacket/async/TaskThreadPoolConfig.java
+++ src/main/java/com/wsloan/redpacket/async/TaskThreadPoolConfig.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 /**
  * Created by EdsionGeng on 2018/4/23.
  */
-//@ConfigurationProperties(prefix = "spring.task.pool")
+@ConfigurationProperties(prefix = "spring.task.pool")
 @Data
 public class TaskThreadPoolConfig {
     private int corePoolSize;
